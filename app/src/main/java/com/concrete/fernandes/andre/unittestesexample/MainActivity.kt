5047918b1b79fdbcf3e4d43791e4b41682d26fe1
cs-andre-fernandes <andre.fernandes@concrete.com.br>
2018-02-06 17:14:28 -0200

    Creating Unit Tests Project - Android

---
+++ app/src/main/java/com/concrete/fernandes/andre/unittestesexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.concrete.fernandes.andre.unittestesexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
